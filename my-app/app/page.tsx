b47004aecfdce1da5c384e6a84f363031b73657b
Muhammad Abdullah <muhammadabdullaham24@gmail.com>
2025-02-08 15:31:43 +0000

    added default tab

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,9 +1,10 @@
 import { OptionTab } from "@/components/option-tabs";
 
+
 export default function Home() {
   return (
-    <div>
-      <OptionTab></OptionTab>
+    <div className="flex flex-col justify-center items-center min-h-screen">
+      <OptionTab />
     </div>
   );
 }
--- my-app/components/option-tabs.tsx
+++ my-app/components/option-tabs.tsx
@@ -1,14 +1,18 @@
+'use client'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { ComboboxForm } from "@/components/input-crypto";
 import { TextareaWithButton } from "./text-box";
-
+import { useState } from "react";
 
 
 export function OptionTab(){
+    const [activeTab, setActiveTab] = useState("ai") // Active tab state
+
     return (
         <div>
-            <Tabs defaultValue="account" className="w-[400px]">
-                <TabsList>
+
+            <Tabs value={activeTab} onValueChange={(value) => setActiveTab(value)}  defaultValue="account" className="w-[400px]">
+                <TabsList className="grid w-full grid-cols-2">
                     <TabsTrigger value="ai">Account</TabsTrigger>
                     <TabsTrigger value="manual">Password</TabsTrigger>
                 </TabsList>
@@ -17,9 +21,10 @@ export function OptionTab(){
                 </TabsContent>
                 <TabsContent value="manual">
                     <ComboboxForm></ComboboxForm>
-
                 </TabsContent>
             </Tabs>
+
+            
         </div>
     )
 }

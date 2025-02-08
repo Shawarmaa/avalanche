bb2e629e67285120fb6e5d1dc708afc4f056cdb4
Muhammad Abdullah <muhammadabdullaham24@gmail.com>
2025-02-08 15:41:06 +0000

    yolo

--- my-app/components/option-tabs.tsx
+++ my-app/components/option-tabs.tsx
@@ -13,8 +13,8 @@ export function OptionTab(){
 
             <Tabs value={activeTab} onValueChange={(value) => setActiveTab(value)}  defaultValue="account" className="w-[400px]">
                 <TabsList className="grid w-full grid-cols-2">
-                    <TabsTrigger value="ai">Account</TabsTrigger>
-                    <TabsTrigger value="manual">Password</TabsTrigger>
+                    <TabsTrigger value="ai">AI</TabsTrigger>
+                    <TabsTrigger value="manual">Manual</TabsTrigger>
                 </TabsList>
                 <TabsContent value="ai">
                     <TextareaWithButton></TextareaWithButton>
